ec0da6553f338d5d50ab8d88cd830a606842c8c0
ThatTSGuy <48391646+ThatTSGuy@users.noreply.github.com>
2022-03-28 19:55:50 -0400

    Update to have block comments consume whitespace
    Fix for previous commit

--- src/tokens.ts
+++ src/tokens.ts
@@ -9,7 +9,7 @@ const singleWordCookware = /#(?<sCookwareName>[^\s]+)/;
 const timer = /~(?<timerName>.*?)(?:{(?<timerQuantity>.*?)(?:%(?<timerUnits>.+))?})/;
 
 export const comment = /--.*/g;
-export const blockComment = /\[-[^]*-\]/g;
+export const blockComment = /\s*\[-[^]*-\]\s*/g;
 
 export const shoppingListToken = /\[(?<name>.+)\](?<items>[^]*?)(?:\n\n|$)/g;
 export const tokens = new RegExp([metadata, multiwordIngredient, singleWordIngredient, multiwordCookware, singleWordCookware, timer].map(r => r.source).join('|'), 'g');

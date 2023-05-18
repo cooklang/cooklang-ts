3e2f98f0c1e8cd8a97ea158c805a38a62428a49b
Jayden Dumouchel <jdumouch@ualberta.ca>
2023-05-17 21:22:43 -0600

    Handle punctuation in single word cookware/ingredients (#15)
    Add tests for singleword punctation

--- src/tokens.ts
+++ src/tokens.ts
@@ -1,15 +1,15 @@
 const metadata = /^>>\s*(?<key>.+?):\s*(?<value>.+)/;
 
-const multiwordIngredient = /@(?<mIngredientName>[^@#~[]+?){(?<mIngredientQuantity>[^]*?)(?:%(?<mIngredientUnits>[^}]+?))?}/;
-const singleWordIngredient = /@(?<sIngredientName>[^\s]+)/;
+const multiwordIngredient = /@(?<mIngredientName>[^@#~[]+?)\{(?<mIngredientQuantity>[^]*?)(?:%(?<mIngredientUnits>[^}]+?))?\}/;
+const singleWordIngredient = /@(?<sIngredientName>[^\s\t\p{Zs}\p{P}]+)/;
 
-const multiwordCookware = /#(?<mCookwareName>[^@#~[]+?){(?<mCookwareQuantity>.*?)}/;
-const singleWordCookware = /#(?<sCookwareName>[^\s]+)/;
+const multiwordCookware = /#(?<mCookwareName>[^@#~[]+?)\{(?<mCookwareQuantity>.*?)\}/;
+const singleWordCookware = /#(?<sCookwareName>[^\s\t\p{Zs}\p{P}]+)/;
 
-const timer = /~(?<timerName>.*?)(?:{(?<timerQuantity>.*?)(?:%(?<timerUnits>.+?))?})/;
+const timer = /~(?<timerName>.*?)(?:\{(?<timerQuantity>.*?)(?:%(?<timerUnits>.+?))?\})/;
 
 export const comment = /--.*/g;
 export const blockComment = /\s*\[\-.*?\-\]\s*/g;
 
 export const shoppingList = /\[(?<name>.+)\]\n(?<items>[^]*?)(?:\n\n|$)/g;
-export const tokens = new RegExp([metadata, multiwordIngredient, singleWordIngredient, multiwordCookware, singleWordCookware, timer].map(r => r.source).join('|'), 'g');
+export const tokens = new RegExp([metadata, multiwordIngredient, singleWordIngredient, multiwordCookware, singleWordCookware, timer].map(r => r.source).join('|'), 'gu');

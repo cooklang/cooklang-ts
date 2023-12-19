24b6752f5bf19df8975d8f38066c4020532cb1a5
Viktor Szépe <viktor@szepe.net>
2023-12-19 00:21:04 +0000

    Fix EC violations

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/**
+ * @type {import('ts-jest/dist/types').InitialOptionsTsJest}
+ */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+    preset: 'ts-jest',
+    testEnvironment: 'node',
 };
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -9,30 +9,30 @@ const testsPath = "./tests";
 const testFiles = fs.readdirSync(testsPath).filter((f) => f.endsWith(".yaml"));
 
 testFiles.forEach((testFile) => {
-  const testYaml = fs.readFileSync(`${testsPath}/${testFile}`, "utf-8");
-  const testData = yaml.parse(testYaml).tests as Record<
-    string,
-    { source: string; result: any }
-  >;
+    const testYaml = fs.readFileSync(`${testsPath}/${testFile}`, "utf-8");
+    const testData = yaml.parse(testYaml).tests as Record<
+        string,
+        { source: string; result: any }
+    >;
 
-  describe(testFile, () => {
-    Object.entries(testData).forEach(([name, testEntry]) => {
-      it(name, () => {
-        const { source, result } = testEntry;
-        const parsed = parser.parse(source);
+    describe(testFile, () => {
+        Object.entries(testData).forEach(([name, testEntry]) => {
+            it(name, () => {
+                const { source, result } = testEntry;
+                const parsed = parser.parse(source);
 
-        const expected = {
-          steps: result.steps,
-          metadata: Array.isArray(result.metadata) ? {} : result.metadata,
-        };
+                const expected = {
+                    steps: result.steps,
+                    metadata: Array.isArray(result.metadata) ? {} : result.metadata,
+                };
 
-        const actual = {
-          steps: parsed.steps,
-          metadata: parsed.metadata,
-        };
+                const actual = {
+                    steps: parsed.steps,
+                    metadata: parsed.metadata,
+                };
 
-        expect(expected).toStrictEqual(actual);
-      });
+                expect(expected).toStrictEqual(actual);
+            });
+        });
     });
-  });
 });

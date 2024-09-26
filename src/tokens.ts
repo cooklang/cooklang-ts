9ea85e98dec0087fb0385ddaf30d53448964ac60
Roger Veciana i Rovira <rveciana@gmail.com>
2024-09-26 20:05:35 +0200

    Add support for multiline block comments (#25)
    Thanks!

--- src/tokens.ts
+++ src/tokens.ts
@@ -9,7 +9,7 @@ const singleWordCookware = /#(?<sCookwareName>[^\s\t\p{Zs}\p{P}]+)/;
 const timer = /~(?<timerName>.*?)(?:\{(?<timerQuantity>.*?)(?:%(?<timerUnits>.+?))?\})/;
 
 export const comment = /--.*/g;
-export const blockComment = /\s*\[\-.*?\-\]\s*/g;
+export const blockComment = /\s*\[\-[\s\S]*?\-\]\s*/g;
 
 export const shoppingList = /\n\s*\[(?<name>.+)\]\n(?<items>[^]*?)(?:\n\n|$)/g;
 export const tokens = new RegExp([metadata, multiwordIngredient, singleWordIngredient, multiwordCookware, singleWordCookware, timer].map(r => r.source).join('|'), 'gu');
